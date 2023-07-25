c0b6874fc39d9d0619c4eb12dec7109fd42c8752
akianzadeh <ali.kianzadeh@sony.com>
2023-07-25 15:23:49 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from './auth.service';
+import {User} from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'heroes-app';
+  user?: User;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authService.getUser()
+      .subscribe(user => this.user = user);
+    // console.log(this.user?.name)
+  }
+
+  logout() {
+    this.authService.logout();
+    this.user = undefined;
+  }
 }
